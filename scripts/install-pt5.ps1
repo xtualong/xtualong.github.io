4bbec9cc670bc4c733c7dbef678038300b8d328d
xtualong <xtualong@users.noreply.github.com>
2026-06-07 22:50:46 +0000

    deploy: xtualong/BlogSource@520293ffec59d38888d5f9fa169bb7bb13a7e6f6

--- scripts/install-pt5.ps1
+++ scripts/install-pt5.ps1
@@ -53,7 +53,8 @@ function pt {
                 Write-Host "ms`$(`$p[2])"
                 `$last = `$m
             } else { Write-Host "`$ts  `$l" }
-        } elseif (`$l -match "timed out|请求超时|无法访问目标主机|一般故障") { Write-Host "`$ts  `$l" -ForegroundColor Red; `$last = -1 }
+        } elseif (`$l -match "timed out|timed out|unreachable|General failure|Request timed out|Destination host") { Write-Host "`$ts  `$l" -ForegroundColor Red; `$last = -1 }
+        elseif (`$l -match "\d+ms" -and `$l -notmatch "TTL=") { Write-Host "`$ts  `$l" -ForegroundColor Red; `$last = -1 }
         else { Write-Host "`$ts  `$l" }
     }
 }
